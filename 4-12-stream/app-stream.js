62fb4dcc9ed0eeffdf86784f4c707ef35809765c
mingeonho1 <als77770@naver.com>
2022-05-31 17:28:06 +0900

    Update app-stream.js

--- 4-12-stream/app-stream.js
+++ 4-12-stream/app-stream.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const data = [];
 
 fs.createReadStream('./file.txt', { // 스트림은 조금조금씩 읽어오기 때문에 이벤트 베이스!
-    highWaterMark: 8, // 기본값 64 kbytes
-    // encoding: 'utf-8',
+    // highWaterMark: 8, // 기본값 64 kbytes
+    encoding: 'utf-8',
 }).on('data', chunk => {
-    // console.log(chunk);
+    console.log(chunk);
     data.push(chunk);
-    console.count('data');
+    // console.count('data');
 }).on('end', () => {
     console.log(data.join(''));
 }).on('error', error => {
